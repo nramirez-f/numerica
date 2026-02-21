1cec5ac6cebb58b3503a1c7aefd1eb9401c3491a
nramirez <nramirez@uma.es>
2026-02-21 18:11:29 +0100

    refactor(roots):improve methosd and make it safety

--- src/numerica/roots/bisection.py
+++ src/numerica/roots/bisection.py
@@ -1,9 +1,10 @@
+import sys
 import numpy as np
 
 def bisection(f, a, b, tol=1e-12, maxiter = 100, debug=False, display_precision=8):
 
     if (f(a)*f(b) > 0.0):
-        raise ValueError(f"No sign change: f(a)={f(a):.{display_precision}e} and f(b)={f(b):.{display_precision}e} have the same sign. Bisection method cannot be applied.")
+        raise ValueError(f"No sign change: f(a)={f(a):.{display_precision}e} and f(b)={f(b):.{display_precision}e} have the same sign. Method cannot be applied.")
 
     if (debug):
         print(f"{'Iter':>5} | {'left_extreme':>15} | {'right_extreme':>15} | {'approx_root':>15} | {'value':>15}")
@@ -22,8 +23,14 @@ def bisection(f, a, b, tol=1e-12, maxiter = 100, debug=False, display_precision=
         if (debug):
             print(f"{n:>5} | {a:>15.{display_precision}e} | {b:>15.{display_precision}e} | {approx_root:>15.{display_precision}e} | {f(approx_root):>15.{display_precision}e}")
 
-        if (f(a) * f(approx_root) < 0.0):
+        if (abs(f(approx_root)) < sys.float_info.epsilon):
+            if (debug):
+                print(f"\nRoot found at x = {approx_root:.{display_precision}e}")
+            return approx_root
+        
+        elif (f(a) * f(approx_root) < 0.0):
             b = approx_root
+            
         else:
             a = approx_root
         
--- src/numerica/roots/regula_falsi.py
+++ src/numerica/roots/regula_falsi.py
@@ -1,11 +1,16 @@
+import sys
 
-def regula_falsi(f, a, b, tol=1e-12, maxiter = 1000, debug=False, display_precision=8):
+def regula_falsi(f, a, b, tol=1e-12, maxiter = 1000, debug=False, stop_criteria='value', display_precision=8):
+
+    if (f(a)*f(b) > 0.0):
+        raise ValueError(f"No sign change: f(a)={f(a):.{display_precision}e} and f(b)={f(b):.{display_precision}e} have the same sign. Method cannot be applied.")
 
     if (debug):
-        print(f"{'Iter':>5} | {'a_n':>15} | {'b_n':>15} | {'root':>15} | {'f(root)':>15}")
+        print(f"{'Iter':>5} | {'left_extreme':>15} | {'right_extreme':>15} | {'approx_root':>15} | {'value':>15}")
         print("-" * 80)
 
-    n = 0
+    n = 1
+    c_old = a
     while (n < (maxiter + 1)):
         
         c = b - f(b)*(b-a)/(f(b) - f(a))
@@ -13,20 +18,31 @@ def regula_falsi(f, a, b, tol=1e-12, maxiter = 1000, debug=False, display_precis
         if (debug):
             print(f"{n:>5} | {a:>15.{display_precision}e} | {b:>15.{display_precision}e} | {c:>15.{display_precision}e} | {f(c):>15.{display_precision}e}")
 
-        if (abs(f(c)) < tol):
+        if (abs(f(c)) < sys.float_info.epsilon):
             if (debug):
-                print(f'\nFound root with tol {tol:.{display_precision}e} at x = {c:.{display_precision}e} with f(x) = {f(c):.{display_precision}e}')
+                print(f"\nRoot found at x = {c:.{display_precision}e}")
             return c
         
-        if (f(a)*f(c) < 0.0):
+        elif (f(a)*f(c) < 0.0):
             b = c
+
         else:
             a = c
+
+        if (stop_criteria == 'interval' and abs(b-a) < tol or
+            stop_criteria == 'heuristic' and abs(c - c_old) < tol or
+            stop_criteria == 'value' and abs(f(c)) < tol):
+            
+            if (debug):
+                print('-' * 80)
+                print(f"\nConvergence criteria achieved. Approx root at x = {c:.{display_precision}e} with value = {f(c):.{display_precision}e}")
+            return c
         
+        c_old = c
         n += 1
 
     if (debug):
         print('-' * 80)
-        print(f'\nMaximum iterations reached. Approximate root at x = {c:.{display_precision}e} with f(x) = {f(c):.{display_precision}e}')
+        print(f'\nMaximum iterations reached. Approximate root at x = {c:.{display_precision}e} with value = {f(c):.{display_precision}e}')
     
     return c
